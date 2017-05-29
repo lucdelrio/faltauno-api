963dd4e56a65550ab473d5f27a4fec790f776e70
lucdelrio <lucasgdelrio93@gmail.com>
2017-05-28 22:13:24 -0300

    Cambia nombre entity postMatch

--- src/main/java/ar/api/faltauno/controladores/MatchController.java
+++ src/main/java/ar/api/faltauno/controladores/MatchController.java
@@ -6,13 +6,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import ar.api.faltauno.modelo.Partido;
+import ar.api.faltauno.modelo.Usuario;
 import ar.api.faltauno.servicios.MatchService;
 
 @RestController
@@ -22,7 +26,7 @@ public class MatchController {
     private MatchService matchService;
 	
     @RequestMapping(value = "/match", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> createMatch(@RequestBody Partido match, UriComponentsBuilder ucBuilder) {
+    public ResponseEntity<Void> postMatch(@RequestBody Partido match, UriComponentsBuilder ucBuilder) {
  
         if (matchService.isMatchExist(match)) {
             return new ResponseEntity<Void>(HttpStatus.CONFLICT);
@@ -35,10 +39,5 @@ public class MatchController {
         return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
     }
     
-    @RequestMapping("/hola")
-    public String hola() {
-    	return "hola";
-    }
-	
 
 }
